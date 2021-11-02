7df179a5105827073ada14f5489850eaac1ec1ab
renko0909 <49751773+renko0909@users.noreply.github.com>
2021-11-01 17:05:26 -0700

    first commit

---
+++ a0521-react-100-babel-jsx/06-practice-class/app.js
@@ -0,0 +1,15 @@
+let category = data.map((result, index) => {
+    return (
+        <Category key={index} data={result} />
+    )
+})
+
+
+ReactDOM.render(
+    <div className="row">
+        <ul className="cat-nav center-align">
+        {category}
+        </ul>
+    </div>, 
+    document.getElementById('root')
+)
